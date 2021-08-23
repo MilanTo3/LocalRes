655319f029608b1a25dbea1c27d2c9343f2fd31b
MilanTo3 <mtomin367@gmail.com>
2021-08-23 15:15:16 +0200

    commit

---
+++ LocalController/ModelClasses/Globals.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalController.ModelClasses
+{
+    public class Globals {
+        public enum UnitType { Solar, Wind, MicroHydro };
+        public enum Control { Local, Remote };
+    }
+}
---
+++ LocalController/ModelClasses/Unit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalController.ModelClasses
+{
+    public class Unit
+    {
+        long id;
+        long numOfUnits;
+        double currentProduct;
+        double maxProduct;
+
+    }
+}
